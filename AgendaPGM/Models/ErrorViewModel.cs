06620e3b9a2beeb77e768e4343479a2e85c174bd
Patricio <pgmonzon@gmail.com>
2019-04-17 15:14:53 -0300

    Agregar archivos de proyecto.

---
+++ AgendaPGM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AgendaPGM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
